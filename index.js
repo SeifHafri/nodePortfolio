8b12e39eab0efb3dcec68e2d7f6c9775d052efdb
[SeifHafri] <[is_hafri@esi.dz]>
2021-09-14 15:41:06 +0100

    intial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+var express = require("express");
+var ejs = require("ejs");
+var app = express();
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+
+console.log(__dirname + "/public");
+
+app.get("/", function (req, res) {
+  res.render("index");
+});
+
+app.listen(process.env.PORT || 3000, function () {
+  console.log("Server is running on port 3000");
+});
