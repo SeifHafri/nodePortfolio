1b8fd4b8855c1ba2f3816421499acd33cef82e23
[SeifHafri] <[is_hafri@esi.dz]>
2021-09-15 09:42:57 +0100

    deployment fix v4

--- index.js
+++ index.js
@@ -4,8 +4,6 @@ var app = express();
 app.set("view engine", "ejs");
 app.use(express.static("./public"));
 
-console.log("./public");
-
 app.get("/", function (req, res) {
   res.render("index");
 });
